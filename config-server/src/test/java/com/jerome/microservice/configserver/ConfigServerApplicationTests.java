1a0d221b62d8d5e397aeb24adfb64336e4f0bf2e
jfantou <fantou,jerome@gmail.com>
2023-07-24 22:44:02 +0900

    first commit

---
+++ config-server/src/test/java/com/jerome/microservice/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jerome.microservice.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
